05b110a81cb843e4820434be1487fa4dda8fa78f
marcialuisa23 <marcia7monteiro@gmail.com>
2023-07-07 12:40:07 +0100

    adds search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
-
+import "./App.css";
+import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      Hello World!
+      <div className="container">
+        <header className="App-header">Dictionary App</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">coded by Marcia Monteiro</footer>
+      </div>
     </div>
   );
 }
